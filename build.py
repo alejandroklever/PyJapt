a2d95c0818e71cc0a78b563bea660d6c2bf16199
Alejandro Klever <alejandroklever4197@gmail.com>
2020-09-20 23:32:48 +0200

    creating a building process

---
+++ build.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+
+with open('pyjapt/__init__.py', 'r') as f:
+    version = re.search(r'__version__ = \'\d\.\d\.\d\'', f.read()).group().replace('__version__ = \'', '')[:-1]
+
+with open('pyproject.toml', 'r') as f:
+    s = f.read()
+
+pattern = re.compile(r'version = "\d\.\d\.\d"')
+s = s.replace(pattern.search(s).group(), f'version = "{version}"')
+
+with open('pyproject.toml', 'w') as f:
+    f.write(s)
+
+os.system('poetry build')
+os.system(f'pip install dist/pyjapt-{version}-py3-none-any.whl')
